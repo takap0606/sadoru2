7b3a59522579c7b854a27431419aaa11beb3a2ed
h1r0 <takap0606@gmail.com>
2016-10-28 18:44:08 +0900

    Initial Commit

---
+++ sadoru2/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  sadoru2
+//
+//  Created by 上野貴大 on 2016/10/28.
+//  Copyright © 2016年 mycompany. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
